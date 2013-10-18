72129194aab75d534bd7c6390be44f82a592feb7
renouve <gabriel_affaire@hotmail.com>
2013-10-18 19:38:08 -0400

    Increase knight value.

--- src/types.h
+++ src/types.h
@@ -166,7 +166,7 @@ enum Value {
   VALUE_ENSURE_INTEGER_SIZE_N = INT_MIN,
 
   PawnValueMg   = 198,   PawnValueEg   = 258,
-  KnightValueMg = 817,   KnightValueEg = 846,
+  KnightValueMg = 834,   KnightValueEg = 846,
   BishopValueMg = 836,   BishopValueEg = 857,
   RookValueMg   = 1270,  RookValueEg   = 1278,
   QueenValueMg  = 2521,  QueenValueEg  = 2558
