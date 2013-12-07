419851fc68030be0d3c5655c5a92ed8e4c46e40f
renouve <gabriel_affaire@hotmail.com>
2013-12-07 13:19:29 -0500

    Depth based easy move.

--- src/search.cpp
+++ src/search.cpp
@@ -441,7 +441,7 @@ namespace {
                 && (   RootMoves.size() == 1
                     || Time::now() - SearchTime > (TimeMgr.available_time() * 20) / 100))
             {
-                Value rBeta = bestValue - 2 * PawnValueMg;
+                Value rBeta = bestValue - int(float(PawnValueMg) * (float(24) / float(depth)));
                 ss->excludedMove = RootMoves[0].pv[0];
                 ss->skipNullMove = true;
                 Value v = search<NonPV>(pos, ss, rBeta - 1, rBeta, (depth - 3) * ONE_PLY, true);
